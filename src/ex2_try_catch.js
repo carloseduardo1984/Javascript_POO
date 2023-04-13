a714d7b398f5415b8819e426ea041a32bcbc0a91
CarlosEduardo1984 <33332202+carloseduardo1984@users.noreply.github.com>
2023-04-13 20:16:49 +0100

    Update ex2_try_catch.js

--- src/ex2_try_catch.js
+++ src/ex2_try_catch.js
@@ -1,15 +1,15 @@
 const produto = { id : 1, nome : 'Grampo', preco : 34.7 }  //EXEMPLO DE TRATATIVA DE EXCESSÃO - ERRO
  
- const produtoFreeze = Object.freeze(produto) 
+ const produtoNovo = new Object(produto) 
  
- produtoFreeze.preco = null 
-
+ 
  try {
     
-    console.log("LINHA 15" + produtoFreeze.preco) // 34.7
-    onsole.log("GLINHA 16" + produtoFreeze.peco) // 34.7
+    console.log("LINHA 15 -> " + produtoNovo.preco) // 34.7
+    onsole.log("GLINHA 16 -> " + produtoNovo.peco) // 34.7
     
   }
   catch(e) {
     console.log("FALHA DETECTADA ") // 
   }
+
